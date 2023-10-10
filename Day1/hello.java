fbee29d018cc981159f97cf5a0b98a0ff11b01ba
Sham Johari <122847705+ShamJohari20@users.noreply.github.com>
2023-10-10 22:58:12 +0530

    Add files via upload

---
+++ Day1/hello.java
@@ -0,0 +1,9 @@
+package Day1;
+
+public class hello {
+	public static void main(String arr[]) {
+		System.out.println("hello java");
+		
+		}
+	}
+
---
+++ Day2/Driver.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class Driver {
+	
+	
+	String name = "Sham";
+	int age = 22;
+	
+	public void drive() {
+		System.out.println("Car is drive");
+		
+	}
+
+}
